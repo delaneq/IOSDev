33fff33b5fba483dda6ec76485816456f6c73b65
Mukan Miras <mukanmiras@Air--Mors.lan>
2024-07-15 22:03:13 +0500

    Created base project, removed storyboard references

---
+++ FoodDelivery/FoodDelivery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodDelivery
+//
+//  Created by Mukan Miras on 15.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
---
+++ TestFoodDelivery/TestFoodDelivery/TestFoodDeliveryApp.swift
@@ -0,0 +1,18 @@
+//
+//  TestFoodDeliveryApp.swift
+//  TestFoodDelivery
+//
+//  Created by Mukan Miras on 15.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestFoodDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            
+        }
+    }
+}
